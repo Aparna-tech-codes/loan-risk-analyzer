e86e419d2722305a33f581cee07a099b4cc2d1b5
Aparna Nikam <aparnanikam95@gmail.com>
2026-06-30 18:07:30 +0530

    dev branch created

--- packages/core/src/config/default-risk-config.ts
+++ packages/core/src/config/default-risk-config.ts
@@ -2,10 +2,7 @@ import { RiskConfig } from "../types/risk-config.types";
 
 export const defaultRiskConfig: RiskConfig = {
   minimumCreditScore: 650,
-
   minimumAge: 21,
-
   maximumAge: 60,
-
   minimumApprovalScore: 60,
 };
