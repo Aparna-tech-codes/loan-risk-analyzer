75e6843b197f24db96e73ee444d83ae32945b59c
Aparna Nikam <aparnanikam95@gmail.com>
2026-06-30 18:27:46 +0530

    feat(core): restructure repository architecture

--- packages/core/src/config/default-risk-config.ts
+++ packages/core/src/config/default-risk-config.ts
@@ -2,7 +2,10 @@ import { RiskConfig } from "../types/risk-config.types";
 
 export const defaultRiskConfig: RiskConfig = {
   minimumCreditScore: 650,
+
   minimumAge: 21,
+
   maximumAge: 60,
+
   minimumApprovalScore: 60,
 };
